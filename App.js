fdddd0beb1e660a1b1f610259e35ed6ed3e37d76
John Sanchez <Johnsanchez2024@u.northwestern.edu>
2023-02-05 15:29:43 -0600

    brought back og code

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import {ImageBackground, StyleSheet, Text, Button, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import {ImageBackground, StyleSheet, Text, Button, View} from 'react-native';
 
 const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'};
 
 export default function App(){
   const [count, setCount] = useState(0);
   return(
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>        
-        <TouchableOpacity
-          style={styles.buttonFacebookStyle}
-          activeOpacity={0.5}>
-            
-          <Image
-            source={{
-              uri:
-                '/images/north-america-map.png',
-            }}
-            style={styles.buttonImageIconStyle}
-          />
-          <View styles={styles.buttonIconSeparatorStyle}/>
-          <Text styles={styles.buttonTextStyle}>
-            Learn more about NA
-          </Text>
-        </TouchableOpacity>
-
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <StatusBar style='auto' />
-          <Text style={styles.text}>Hi IHTU!</Text>
-          <Text style={styles.count}>You clicked {count} times</Text>
-          <Button
-            onPress={() => setCount(count + 1)}
-            title="Click me!"
-          />
-        </ImageBackground>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>        
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <StatusBar style='auto' />
+        <Text style={styles.text}>Hi IHTU!</Text>
+        <Text style={styles.count}>You clicked {count} times</Text>
+        <Button
+          onPress={() => setCount(count + 1)}
+          title="Click me!"
+        />
+      </ImageBackground>
+    </View>
   )
 };
 
@@ -55,33 +36,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: '#000000c0',
-  },
-  buttonFacebookStyle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#485a96',
-    borderWidth: 0.5,
-    borderColor: '#fff',
-    height: 40,
-    borderRadius: 5,
-    margin: 5,
-  },
-  buttonImageIconStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: 'stretch',
-  },
-  buttonTextStyle: {
-    color: '#fff',
-    marginBottom: 4,
-    marginLeft: 10,
-  },
-  buttonIconSeparatorStyle: {
-    backgroundColor: '#fff',
-    width: 1,
-    height: 40,
   },
   text: {
     color: 'white',
