58284d003c7783f28552a2e3e302c2f599a1d269
anyabdch <anya.bardach@gmail.com>
2023-02-08 20:15:42 -0600

    multipage attempt

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import {ImageBackground, StyleSheet, Text, Button, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
-
-const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'};
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Test
-export default function App(){
+
+const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'}
+
+function HomeScreen(){
   const [count, setCount] = useState(0);
   return(
     <SafeAreaView style={{ flex: 1 }}>
@@ -39,6 +42,19 @@ export default function App(){
     </SafeAreaView>
   )
 };
+
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={HomeScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -92,3 +108,94 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000c0',
   },
 });
+
+// const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'};
+
+// // Test
+// export default function App(){
+//   const [count, setCount] = useState(0);
+//   return(
+//     <SafeAreaView style={{ flex: 1 }}>
+//       <View style={styles.container}>        
+//         <TouchableOpacity
+//           style={styles.buttonFacebookStyle}
+//           activeOpacity={0.5}>
+//           <Image
+//             source={{
+//               uri:
+//                 '/images/north-america-map.png',
+//             }}
+//             style={styles.buttonImageIconStyle}
+//           />
+//           <View styles={styles.buttonIconSeparatorStyle}/>
+//           <Text styles={styles.buttonTextStyle}>
+//             Learn more about NA
+//           </Text>
+//         </TouchableOpacity>
+
+//         <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+//           <StatusBar style='auto' />
+//           <Text style={styles.text}>Hi IHTU!</Text>
+//           <Text style={styles.count}>You clicked {count} times</Text>
+//           <Button
+//             onPress={() => setCount(count + 1)}
+//             title="Click me!"
+//           />
+//         </ImageBackground>
+//       </View>
+//     </SafeAreaView>
+//   )
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   image: {
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   count: {
+//     color: 'white',
+//     fontSize: 12,
+//     lineHeight: 84,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//     backgroundColor: '#000000c0',
+//   },
+//   buttonFacebookStyle: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     backgroundColor: '#485a96',
+//     borderWidth: 0.5,
+//     borderColor: '#fff',
+//     height: 40,
+//     borderRadius: 5,
+//     margin: 5,
+//   },
+//   buttonImageIconStyle: {
+//     padding: 10,
+//     margin: 5,
+//     height: 25,
+//     width: 25,
+//     resizeMode: 'stretch',
+//   },
+//   buttonTextStyle: {
+//     color: '#fff',
+//     marginBottom: 4,
+//     marginLeft: 10,
+//   },
+//   buttonIconSeparatorStyle: {
+//     backgroundColor: '#fff',
+//     width: 1,
+//     height: 40,
+//   },
+//   text: {
+//     color: 'white',
+//     fontSize: 42,
+//     lineHeight: 84,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//     backgroundColor: '#000000c0',
+//   },
+// });
