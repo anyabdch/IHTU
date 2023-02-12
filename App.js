2c156647a57929c168f011aad156961aadc97ffa
Anya <116769112+anyabdch@users.noreply.github.com>
2023-02-12 16:02:27 -0600

    Update App.js

--- App.js
+++ App.js
@@ -10,72 +10,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'}
 
-function HomeScreen(){
-  const [count, setCount] = useState(0);
-  Orientation.unlockAllOrientations;
-  Orientation.lockToLandscape;
-
-  // //onPress To Navigate
-  // const onPress = () => {
-  //   props.navigation.navigate('ScreenTwo');
-  // };
-
-  return(
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>        
-        <TouchableOpacity
-          style={styles.buttonFacebookStyle}
-          activeOpacity={0.5}>
-          <Image
-            source={image}
-            style={styles.buttonImageIconStyle}
-          />
-          <View styles={styles.buttonIconSeparatorStyle}/>
-          <Text styles={styles.buttonTextStyle}>
-            Learn more about NA
-          </Text>
-        </TouchableOpacity>
-
-        {/* Background image */}
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <StatusBar style='auto' />
-          <Text style={styles.text}>Hi IHTU!</Text>
-          <Text style={styles.count}>You clicked {count} times</Text>
-          <Button
-            onPress={() => setCount(count + 1)}
-            title="Click me!"
-          />
-        </ImageBackground>
-      </View>
-    </SafeAreaView>
-  )
-};
-
 //Screen One
-const ScreenOne = props => {
+const HomeScreen = props => {
   const [count, setCount] = useState(0);
   Orientation.unlockAllOrientations;
   Orientation.lockToLandscape;
-
-  //onPress To Navigate
-  const onPress = () => {
-    props.navigation.navigate('ScreenTwo');
-  };
 
   return (
-      <View style={styles.container}>        
-        {/* <TouchableOpacity
-          style={styles.buttonFacebookStyle}
-          activeOpacity={0.5}>
-          <Image
-            source={image}
-            style={styles.buttonImageIconStyle}
-          />
-          <View styles={styles.buttonIconSeparatorStyle}/>
-          <Text styles={styles.buttonTextStyle}>
-            Learn more about NA
-          </Text>
-        </TouchableOpacity> */}
+      <View style={styles.container}>       
 
         {/* Background image */}
         <ImageBackground source={image} resizeMode="cover" style={styles.image}>
@@ -92,11 +34,6 @@ const ScreenOne = props => {
           />
         </ImageBackground>
       </View>
-    // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    //   <TouchableOpacity onPress={onPress}>
-    //     <Text>Hello From Screen One</Text>
-    //   </TouchableOpacity>
-    // </View>
   );
 };
 
@@ -115,10 +52,8 @@ export default function App(){
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="ScreenOne" component={ScreenOne} />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ScreenTwo" component={ScreenTwo} />
-        <Stack.Screen name="home" component={HomeScreen} />
-
       </Stack.Navigator>
     </NavigationContainer>
   )
@@ -149,94 +84,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000c0',
   },
 });
-
-// const image = {uri: 'https://wallpapercave.com/wp/wp5136232.jpg'};
-
-// // Test
-// export default function App(){
-//   const [count, setCount] = useState(0);
-//   return(
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <View style={styles.container}>        
-//         <TouchableOpacity
-//           style={styles.buttonFacebookStyle}
-//           activeOpacity={0.5}>
-//           <Image
-//             source={{
-//               uri:
-//                 '/images/north-america-map.png',
-//             }}
-//             style={styles.buttonImageIconStyle}
-//           />
-//           <View styles={styles.buttonIconSeparatorStyle}/>
-//           <Text styles={styles.buttonTextStyle}>
-//             Learn more about NA
-//           </Text>
-//         </TouchableOpacity>
-
-//         <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-//           <StatusBar style='auto' />
-//           <Text style={styles.text}>Hi IHTU!</Text>
-//           <Text style={styles.count}>You clicked {count} times</Text>
-//           <Button
-//             onPress={() => setCount(count + 1)}
-//             title="Click me!"
-//           />
-//         </ImageBackground>
-//       </View>
-//     </SafeAreaView>
-//   )
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   image: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-//   count: {
-//     color: 'white',
-//     fontSize: 12,
-//     lineHeight: 84,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     backgroundColor: '#000000c0',
-//   },
-//   buttonFacebookStyle: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     backgroundColor: '#485a96',
-//     borderWidth: 0.5,
-//     borderColor: '#fff',
-//     height: 40,
-//     borderRadius: 5,
-//     margin: 5,
-//   },
-//   buttonImageIconStyle: {
-//     padding: 10,
-//     margin: 5,
-//     height: 25,
-//     width: 25,
-//     resizeMode: 'stretch',
-//   },
-//   buttonTextStyle: {
-//     color: '#fff',
-//     marginBottom: 4,
-//     marginLeft: 10,
-//   },
-//   buttonIconSeparatorStyle: {
-//     backgroundColor: '#fff',
-//     width: 1,
-//     height: 40,
-//   },
-//   text: {
-//     color: 'white',
-//     fontSize: 42,
-//     lineHeight: 84,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     backgroundColor: '#000000c0',
-//   },
-// });
